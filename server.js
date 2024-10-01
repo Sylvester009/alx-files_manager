b3eab80c5b5b9d23eb4d9e73f53ffea94a557f15
Sylvester009 <sasy260902@gmail.com>
2024-10-01 23:35:51 +0100

    first API updated2

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 
 app.use('/', router);
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${port}`);
 });
 
 export default app;
