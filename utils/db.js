b41bab8e960b44cb63025aede50d1b2840365481
Sylvester009 <sasy260902@gmail.com>
2024-10-01 22:15:58 +0100

    database file

---
+++ utils/db.js
@@ -0,0 +1,37 @@
+const { MongoClient } = require('mongodb');
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || '27017';
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+
+class DBClient {
+  constructor() {
+    const url = `mongodb://${DB_HOST}:${DB_PORT}`;
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+
+    this.client
+      .connect()
+      .then(() => {
+        this.db = this.client.db(DB_DATABASE);
+      })
+      .catch((err) => {
+        console.error('connection error:', err);
+      });
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
